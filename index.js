d2bdb279dd802571ca0dd4d8cbfe7a8e113541b7
Waffle <88282807+WaffleKone@users.noreply.github.com>
2022-02-10 00:56:46 -0500

    made the result show on the webpage instead of the console

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 let myArr = [12, 7, 32, 48, 500, 504, 1]
-
+let myArrChanged = myArr
 let timesClicked = 0
 
 let largestNum = myArr[0]
 let secondLargestNum = myArr[0]
 
 let header = document.createElement('h1')
-header.textContent = 'Click Me! also check console lol'
+header.textContent = 'Click Me!'
 document.body.append(header)
 
-console.log ("This is the current array:", myArr)
+let paragraph = document.createElement('p')
+paragraph.textContent = ('The array is ' + myArr)
+paragraph.style.fontSize = 20
+document.body.append(paragraph)
 
+let LargFound = document.createElement('p')
+LargFound.style.fontSize = 20
+
+
+console.log ("This is the current array:", myArr)
+// event listeners for the header element
 header.addEventListener('click', secondLargFinder)
 
 header.addEventListener('click', changeText)
@@ -21,7 +30,7 @@ header.addEventListener('mouseover', function() {
 header.addEventListener('mouseout', function() {
   header.style.color = 'black'
 })
-
+// Below is the function used to sort through the array and find the second largest number in it, then print that to console and change html text to match it.
 function secondLargFinder() {
     console.log('secondLargFinder was called!')
 for (i=0; i<myArr.length; i++) {
@@ -35,7 +44,11 @@ for (i=0; i<myArr.length; i++) {
     }
 }
 console.log(secondLargestNum)
+document.body.append(LargFound)
+LargFound.textContent = ('The second largest number is ' + secondLargestNum)
 }
+
+// This function changes the text of elements it is called to so it can track how many times it has been clicked.
 function changeText(e) {
   if (timesClicked == 68) {
     timesClicked += 1
